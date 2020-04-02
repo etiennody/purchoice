4e726e853e0d4d2791a88342245eebd9cff1ac02
Jody Etienne <etiennody@gmail.com>
2020-04-02 15:21:46 +0200

    Feature/create models is done ! (#3)
    * add relationship
    
    * fix foreignkey
    
    * remove __repr__ () method (is optional)
    
    * add default database constants definition
    
    * modify backref in product class
    
    * remove constants file
    
    * modify mysql engine
    
    * add cryptography package
    
    * update hash after having installed cryptography
    
    * add length field on String in additives attribut
    
    * define enum in nutrition grade
    
    * update generic_name, traces, allergens
    
    * update enum in lower case
    
    * remove table builders
    
    * move create_engine to creating tables

--- models.py
+++ models.py
@@ -1,111 +1,82 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Enum, Integer, String
-
-# Engine configuration to connect database with PyMySQL
-engine = create_engine(
-    "mysql+pymysql:purbeurre/purbeurre@localhost/purchoice", echo=True
-)
+from sqlalchemy import Table, Column, Enum, Integer, String, ForeignKey
 
 # Construct a base class for declarative class definitions
 Base = declarative_base()
+
+# Create an association for category and product in a ManyToMany relationship
+cat_prod_asso_table = Table(
+    "cat_prod_asso",
+    Base.metadata,
+    Column("category_id", Integer, ForeignKey("category.id")),
+    Column("product_id", Integer, ForeignKey("product.id")),
+)
+
+# Create an association for brand and product in a ManyToMany relationship
+brand_prod_asso_table = Table(
+    "brand_prod_asso",
+    Base.metadata,
+    Column("brand_id", Integer, ForeignKey("brand.id")),
+    Column("product_id", Integer, ForeignKey("product.id")),
+)
 
 
 # Details about category table to which is mapping
 class Category(Base):
     __tablename__ = "category"
-
     id = Column(Integer, primary_key=True)
     category_name = Column(String(100))
-
-    def __repr__(self):
-        return "<Category(category_name='%s')>" % (self.category_name)
 
 
 # Details about store table to which is mapping
 class Store(Base):
     __tablename__ = "store"
-
     id = Column(Integer, primary_key=True)
     store_name = Column(String(100))
-
-    def __repr__(self):
-        return "<Store(store_name='%s')>" % (self.store_name)
 
 
 # Details about brand table to which is mapping
 class Brand(Base):
     __tablename__ = "brand"
-
     id = Column(Integer, primary_key=True)
     brand_name = Column(String(100))
-
-    def __repr__(self):
-        return "<Brand(brand_name='%s')>" % (self.brand_name)
 
 
 # Details about product table to which is mapping
 class Product(Base):
     __tablename__ = "product"
-
     id = Column(Integer, primary_key=True)
     product_name = Column(String(100))
-    description = Column(String(1000))
+    generic_name = Column(String(1000))
     url = Column(String(500))
-    nutrition_grade_fr = Column(Enum)
-    ingredients_text = Column(String(1000))
-    additives = Column(String())
+    nutrition_grade_fr = Column(Enum("a", "b", "c", "d", "e"))
+    ingredients_text_with_allergens = Column(String(1000))
+    traces = Column(String(300))
+    additives = Column(String(500))
     ingredients_from_palm_oil_n = Column(Integer)
-    traces = Column(String(100))
-
-    def __repr__(self):
-        return (
-            "<Product(product_name='%s', description='%s', url='%s', nutrition_grade_fr='%s', ingredients_text='%s', additives='%s', ingredients_from_palm_oil_n='%s', traces='%s')>"
-            % (
-                self.product_name,
-                self.description,
-                self.url,
-                self.nutrition_grade_fr,
-                self.ingredients_text,
-                self.additives,
-                self.ingredients_from_palm_oil_n,
-                self.traces,
-            )
-        )
+    categories = relationship("Category", secondary="cat_prod_asso", backref="products")
+    brands = relationship("Brand", secondary="brand_prod_asso", backref="products")
 
 
 # Details about product_store table to which is mapping
 class ProductStore(Base):
     __tablename__ = "product_store"
-
     id = Column(Integer, primary_key=True)
-    store_id = Column(Integer)
-    product_store_id = Column(Integer)
-
-    def __repr__(self):
-        return "<ProductStore(store_id='%s', product_store_id='%s')>" % (
-            self.store_id,
-            self.product_store_id,
-        )
+    store_id = Column(Integer, ForeignKey("store.id"))
+    product_store_id = Column(Integer, ForeignKey("product.id"))
+    store = relationship("Store", backref="product_stores")
+    product_store = relationship("Product", backref="products")
 
 
 # Details about favorite table to which is mapping
 class Favorite(Base):
     __tablename__ = "favorite"
-
     id = Column(Integer, primary_key=True)
-    product_id = Column(Integer)
-    product_substitut_id = Column(Integer)
-
-    def __repr__(self):
-        return "<Favorite(product_id='%s', product_substitut_id='%s')>" % (
-            self.product_id,
-            self.product_substitut_id,
-        )
-
-
-# Create a schema using metadata to issue CREATE TABLE statements
-Base.metadata.create_all(engine)
+    product_id = Column(Integer, ForeignKey("product.id"))
+    product_substitut_id = Column(Integer, ForeignKey("product_store.id"))
+    product = relationship("Product", backref="favorites")
+    product_substitut = relationship("Product", backref="favorite_substituts")
