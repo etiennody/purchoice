59ba2a86908fdb1228beb9dd98ae044517bf3795
Jody Etienne <etiennody@gmail.com>
2020-04-13 18:52:10 +0200

    add list categories in constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+#! usr/bin/env Python3
+# coding: utf-8
+
+# define list categories for purchoice app
+CATEGORY_SELECTED = (
+    "conserves",
+    "epiceries",
+    "produits-a-tartiner",
+    "plats-prepares",
+    "produits-laitiers",
+    "snacks",
+)
