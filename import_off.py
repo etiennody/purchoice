8c4c3fd3e8c06b2484494311e2da3e5e4059bb49
Jody Etienne <etiennody@gmail.com>
2020-04-13 18:54:45 +0200

    fix import off (modules, imports method)

--- import_off.py
+++ import_off.py
@@ -6,87 +6,49 @@
 import requests
 import json
 
-from build_database import BuildDatabase
+from constants import CATEGORY_SELECTED
+from purchoice_database import PurchoiceDatabase
 
 
 class ImportOff:
     """ImportOff class download all the data from OpenFood Facts API"""
 
-    def __init__(self):
+    def __init__(self, category):
         self.url = "https://fr.openfoodfacts.org//cgi/search.pl?"
         self.params = {
             "action": "process",
             "tagtype_0": "categories",
             "tag_contains_0": "contains",
-            "tag_0": "desserts_de_riz",
+            "tag_0": category,
+            "sort_by": "unique_scans_n",
+            "page_size": 500,
             "json": 1,
         }
-        self.db = BuildDatabase()
+        self.db = PurchoiceDatabase()
+        self.category = category
 
     def get_off(self):
-        self.r = requests.get(self.url, params=self.params)
-
-    def transform_data(self):
-        """Transform the json file in dict."""
-        self.data = json.loads(self.r)
-
-    def _gen_product(self):
-        for item in self.data["products"]:
-            self.product_name = item["product_name_fr"]
-            self.generic_name = item["generic_name"]
-            self.url = item["url"]
-            self.nutrition_grade_fr = item["nutrition_grade_fr"]
-            self.ingredients_text_with_allergens = item[
-                "ingredients_text_with_allergens"
-            ]
-            self.traces = item["traces"]
-            self.additives_n = item["additives_n"]
-            self.ingredients_from_palm_oil_n = item[
-                "ingredients_from_palm_oil_n"
-            ]
-
-    def _gen_store(self):
-        for item in self.data["products"]:
-            self.store_name = item["stores"]
-
-    def _gen_category(self):
-        for item in self.data["products"]:
-            self.category_name = item["categories"]
-
-    def _gen_brand(self):
-        for item in self.data["products"]:
-            self.brand_name = item["brands"]
-
-    def import_products(self):
-        self.products = list(self._gen_product())
-        self.db.add_product(self.products)
-
-    def import_categories(self):
-        self.categories = list(self._gen_category())
-        self.db.add_category(self.categories)
-
-    def import_brands(self):
-        self.brands = list(self._gen_brand())
-        self.db.add_brand(self.brands)
-
-    def import_stores(self):
-        self.stores = list(self._gen_store)
-        self.db.add_store(self.stores)
+        response = requests.get(self.url, params=self.params)
+        if response.status_code == 200:
+            return json.loads(response.content)["products"]
 
     def import_by_category(self):
-        self.get_off()
-        self.transform_data()
-        self.import_brands()
-        self.import_categories()
-        self.import_products()
-        self.import_stores()
+        self.db.add_category(self.category)
+        products = self.get_off()
+        for product in products:
+            self.db.add_product(product)
+            self.db.add_brand(product.get("brands"))
+            self.db.add_store(product.get("stores"))
 
 
-def main(self):
-    self.db.truncate_tables()
-    self.import_off = ImportOff()
-    self.import_off(self.import_by_category)
+def main():
+    reset = PurchoiceDatabase()
+    reset.truncate_tables()
+    for category in CATEGORY_SELECTED:
+        import_off = ImportOff(category)
+        import_off.import_by_category()
+        continue
 
 
 if __name__ == "__main__":
-    main(self=main)
+    main()
