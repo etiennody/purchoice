18b60d2d653d3a7f16d2a61513230404ead6968a
Jody Etienne <etiennody@gmail.com>
2020-05-03 00:03:54 +0200

    fix get substitute + save in progress

--- purchoice_database.py
+++ purchoice_database.py
@@ -1,10 +1,10 @@
 #! usr/bin/python3
 # coding: utf-8
 
-from models import Base
-from models import Category, Store, Brand, Product
 from os import environ
-from sqlalchemy import create_engine, desc, asc
+from models import Base
+from models import Category, Store, Brand, Product, Favorite, cat_prod_asso
+from sqlalchemy import create_engine, desc, asc, or_
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
 
@@ -29,7 +29,7 @@ class PurchoiceDatabase:
         # create all tables
         Base.metadata.create_all(self.engine)
         # create a Session
-        self.create_session = sessionmaker(bind=self.engine)
+        self.create_session = sessionmaker(bind=self.engine, autoflush=False)
         return self.create_session()
 
     def get_or_create(self, model, **kwargs):
@@ -56,22 +56,30 @@ class PurchoiceDatabase:
         self.session.query(Product).delete()
         self.session.commit()
 
-    def get_healthy_products(self, product_id):
+    def get_healthy_products(self, product):
         """Extract products object from database
         We found healthy products where nutrition grade is equal than a
         and any presence of additives.
         """
-        return self.session.query(Product). \
-            join(Category.category_id). \
-            filter(Product.product_id != product_id). \
-            filter(Product.nutrition_grade_fr <= "b"). \
-            filter(Product.additives_n == 0). \
-            order_by(desc(Product.nutrition_grade_fr)). \
-            order_by(asc(Product.additives_n))
+        category_ids = [c.category_id for c in product.categories]
+        substitute = self.session.query(Product). \
+            join(cat_prod_asso). \
+            filter(cat_prod_asso.c.category_id.in_(category_ids)).\
+            filter(Product.product_id != product.product_id). \
+            filter(or_(
+                Product.nutrition_grade_fr < product.nutrition_grade_fr,
+                Product.additives_n < product.additives_n)
+            ). \
+            order_by(Product.nutrition_grade_fr). \
+            order_by(Product.additives_n)
+        return substitute.first()
+
+    def get_substitute_for_product(self, product_id):
+        return None
 
     def get_product_by_id(self, product_id):
         product = self.session.query(Product). \
-            filter(Product.product_id == product_id).limit(10)
+            filter(Product.product_id == product_id).one()
         return product
 
     def add_product(self, product_dict):
@@ -83,7 +91,7 @@ class PurchoiceDatabase:
             nutrition_grade_fr=product_dict.get("nutrition_grade_fr"),
             ingredients_text_fr=product_dict.get("ingredients_text"),
             additives_n=product_dict.get("additives_n")
-            )
+        )
         self.session.add(product)
         self.session.commit()
         return product
@@ -96,14 +104,14 @@ class PurchoiceDatabase:
         category = self.session.query(Category). \
             filter(Category.category_id == category_id). \
             order_by(func.rand()).one()
-        return category.products
+        return category.products.limit(15)
 
     def add_category(self, category):
         """Insert category and commit the record in database"""
         c = self.get_or_create(
             Category,
             category_name=category.strip().upper()
-            )
+        )
         self.session.commit()
         return c
 
@@ -124,3 +132,19 @@ class PurchoiceDatabase:
         )
         self.session.commit()
         return s
+
+    def save_substitute(self, product, substitute):
+        favorite = Favorite(
+            product_id=product,
+            product_substitute_id=substitute
+        )
+        self.session.add(favorite)
+        self.session.commit()
+
+    # def get_favorites(self):
+    #     """
+    #     show_favorites method display a list of substituted produts
+    #     of the Favorite table.
+    #     """
+    #     favorites = self.session.query(Favorite).all()
+    #     return favorites
